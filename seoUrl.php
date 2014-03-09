4996e6114210bc20d8829b717ac58cbcfcad23ef
Dylan <locvfx@gmail.com>
2014-03-09 14:25:59 +0800

    First upload
    This is the first time sync to server

---
+++ seoUrl.php
@@ -0,0 +1,21 @@
+<?php
+
+/** 
+  * Return URL-Friendly string slug
+  * @param string $string 
+  * @return string 
+  */
+
+function seoUrl($string) {
+    //Unwanted:  {UPPERCASE} ; / ? : @ & = + $ , . ! ~ * ' ( )
+    $string = strtolower($string);
+    //Strip any unwanted characters
+    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+    //Clean multiple dashes or whitespaces
+    $string = preg_replace("/[\s-]+/", " ", $string);
+    //Convert whitespaces and underscore to dash
+    $string = preg_replace("/[\s_]/", "-", $string);
+    return $string;
+}
+
+?>
